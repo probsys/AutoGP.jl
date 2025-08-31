14892b75178ff2d9f74d65cba16cb97edc9b825c
Feras Saad <fsaad@cmu.edu>
2025-08-30 21:14:30 -0400

    Make a docs-specific Project.toml file

--- docs/make.jl
+++ docs/make.jl
@@ -12,6 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Add AutoGP as a Dev package.
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
+Pkg.instantiate()
+
 using Documenter
 using AutoGP
 
